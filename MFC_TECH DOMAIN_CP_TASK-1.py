0f81c1f00a239f7b609e346129a0a800dd85076e
architnigam <architnigam72@gmail.com>
2025-03-04 21:56:53 +0530

    Update MFC_TECH DOMAIN_CP_TASK-1.py

--- MFC_TECH DOMAIN_CP_TASK-1.py
+++ MFC_TECH DOMAIN_CP_TASK-1.py
@@ -13,10 +13,19 @@ def ss(f):
             if f[j] < f[b]:
                 b = j
         f[i], f[b] = f[b], f[i]
+def iss(c):
+    n = len(c)
+    for i in range(1, n):
+        key = c[i]
+        j = i - 1
+        while j >= 0 and c[j] > key:
+            c[j + 1] = c[j]
+            j -= 1
+        c[j + 1] = key
 def mt(sf, d):
-    st = time.time()
+    st = time.perf_counter_ns()
     sf(d)
-    et = time.time()
+    et = time.perf_counter_ns()
     return et - st
 size = int(input("Enter the number of elements: "))
 rl = []
@@ -25,9 +34,19 @@ for _ in range(size):
     rl.append(int(input()))
 bl = rl[:]
 sl = rl[:]
+il = rl[:]
 tb = mt(bs, bl)
 ts = mt(ss, sl)
-print(f"Bubble Sort Time: {tb:.3f} sec")
-print(f"Selection Sort Time: {ts:.3f} sec")
+ti = mt(iss, il)
+print(f"Bubble Sort Time: {tb:.3f} nanosecs")
+print(f"Selection Sort Time: {ts:.3f} nanosecs")
+print(f"Insertion Sort Time: {ti:.3f} nanosecs")
 print("Sorted List using Bubble Sort:", bl)
 print("Sorted List using Selection Sort:", sl)
+print("Sorted List using Insertion Sort:", il)
+if tb < ts and tb < ti:
+    print("On comparing, Bubble Sort is the fastest! 👍")
+elif ts < tb and ts < ti:
+    print("On comparing, Selection Sort is the fastest! 👍")
+else:
+    print("On comparing, Insertion Sort is the fastest! 👍")
